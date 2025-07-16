b47d7dcaec5d1583ac247720882f17f9e4de7a51
Charith M.Rajapaksha <rmcmrajapaksha@students.nsbm.ac.lk>
2025-07-16 15:11:31 +0530

    Update index.js

--- oasis-backend/index.js
+++ oasis-backend/index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3001; // A port for your backend
 
-app.get('/api', (req, res) => {
-  res.send('Hello from the backend!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
